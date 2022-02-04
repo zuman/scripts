e9239c090b904f099bbd2172eb2727740a1bc3e2
Alien Zap <syed.zuman.007@gmail.com>
2022-02-05 00:28:53 +0530

    add user ssh key...

--- cloud/setup.sh
+++ cloud/setup.sh
@@ -4,12 +4,10 @@ user="${1:-zap}"
 password="${2:-password}"
 defaultuser="${3:-ubuntu}"
 echo "hi ${user}"
-
 # Enable Universal Firewall
 # ufw enable
 # ufw allow ssh
 # systemctl restart ufw
-
 # add new user and copy ssh key
 # useradd -m $user
 # echo "$user:$password" | chpasswd
