1a4b58d1ca3ba1362cffded9077e4ef993374df0
Alien Zap <syed.zuman.007@gmail.com>
2022-02-04 23:58:52 +0530

    add user

--- cloud/setup.sh
+++ cloud/setup.sh
@@ -1,3 +1,12 @@
+# Update everything
 apt update && apt upgrade -y
 user="${1:-zap}"
 echo "hi ${user}"
+
+# # Enable Universal Firewall
+# ufw enable
+# ufw allow ssh
+# systemctl restart ufw
+
+# add new user
+useradd -m $user
