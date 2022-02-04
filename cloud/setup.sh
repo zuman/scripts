884e2cb01bae2749ea347b77703ce90b1e109465
Alien Zap <syed.zuman.007@gmail.com>
2022-02-05 00:53:22 +0530

    Replace default user with another

--- cloud/setup.sh
+++ cloud/setup.sh
@@ -1,17 +1,25 @@
-# Update everything
 apt update && apt upgrade -y
 user="${1:-zap}"
 password="${2:-password}"
 defaultuser="${3:-ubuntu}"
-echo "hello ${user}"
-# Enable Universal Firewall
-# ufw enable
-# ufw allow ssh
-# systemctl restart ufw
-# add new user and copy ssh key
-# useradd -m $user
-# echo "$user:$password" | chpasswd
-echo "Adding new user $user"
-mkdir /home/$user/.ssh
-cp /home/$defaultuser/.ssh/authorized_keys /home/$user/.ssh/
+echo "Hello ${user}"
 
+echo "Enable Universal Firewall"
+ufw enable
+ufw allow ssh
+systemctl restart ufw
+
+echo "Set up $user"
+useradd -m $user
+chsh -s /bin/bash $user
+echo "$user:$password" | chpasswd
+mkdir /home/$user/.ssh
+cp /home/$defaultuser/.bashrc /home/$user/.bashrc
+cp /home/$defaultuser/.profile /home/$user/.profile
+cp /home/$defaultuser/.ssh/authorized_keys /home/$user/.ssh/
+chown -R $user:$user /home/$user
+echo "$user ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+echo "Remove $defaultuser from sudoers"
+sed -i "/$defaultuser ALL=(ALL) NOPASSWD:ALL/d" /etc/sudoers
+userdel -r $defaultuser
