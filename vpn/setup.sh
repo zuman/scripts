d75dee93ad4d357304770e858b9922acea7ee1a0
Zuman mac <syed.zuman.007@gmail.com>
2022-03-25 11:55:27 +0530

    docker-compose command

--- vpn/setup.sh
+++ vpn/setup.sh
@@ -7,3 +7,4 @@ sudo chown $user:$user /opt/wireguard-server
 cp docker-compose.yaml /opt/wireguard-server/docker-compose.yaml
 sed -i "/PUID/c\      - PUID=$(id -u)" /opt/wireguard-server/docker-compose.yaml
 sed -i "/PGID/c\      - PGID=$(id -u)" /opt/wireguard-server/docker-compose.yaml
+sudo docker-compose -f /opt/wireguard-server/docker-compose.yaml up -d
