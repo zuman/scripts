977ec7b9a2b51700fce23cc6b81db074b29f85a2
Zuman mac <syed.zuman.007@gmail.com>
2022-03-25 17:41:25 +0530

    vpn setup fix

--- home/packages.sh
+++ home/packages.sh
@@ -1,8 +1,5 @@
 echo "Setup Docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
-groupadd docker
-user=$(whoami)
-sudo usermod -aG docker $user
 
 sudo apt install net-tools openresolv -y
--- vpn/setup.sh
+++ vpn/setup.sh
@@ -1,5 +1,7 @@
 sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common docker-compose -y
 user=$(whoami)
+groupadd docker
+sudo usermod -aG docker $user
 sudo mkdir /opt/wireguard-server
 sudo chown $user:$user /opt/wireguard-server
 cp docker-compose.yaml /opt/wireguard-server/docker-compose.yaml
