e24bc7ba87119b47de1d8ded46d46aa66df271b2
Zuman mac <syed.zuman.007@gmail.com>
2022-05-19 12:21:53 +0530

    move ufw to packages

--- cloud/setup.sh
+++ cloud/setup.sh
@@ -9,12 +9,6 @@ password="${2:-password}"
 defaultuser="${3:-ubuntu}"
 echo "Hello ${user}"
 
-# echo "Enable Universal Firewall"
-# sed -i 's/^\[Unit\]/\[Unit\]\nAfter=netfilter-persistent.service/' /lib/systemd/system/ufw.service
-# echo "y" | sudo ufw enable
-# ufw allow ssh
-# systemctl restart ufw
-
 echo "Set up $user"
 useradd -m $user
 chsh -s /bin/bash $user
--- home/packages.sh
+++ home/packages.sh
@@ -3,3 +3,9 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
 
 sudo apt install net-tools openresolv -y
+
+echo "Enable Universal Firewall"
+sed -i 's/^\[Unit\]/\[Unit\]\nAfter=netfilter-persistent.service/' /lib/systemd/system/ufw.service
+echo "y" | sudo ufw enable
+ufw allow ssh
+systemctl restart ufw
