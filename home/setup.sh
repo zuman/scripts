d54c7294d65d0f2c5c393411b41657ad774598fd
Zuman Mac <syed.zuman.007@gmail.com>
2022-11-16 11:52:11 +0530

    Fix setup script

--- home/setup.sh
+++ home/setup.sh
@@ -1,5 +1,5 @@
-cp -r ../scripts ~
-cp .profile ~
-sed -i 's/PS1=.*/PS1="${debian_chroot:+($debian_chroot)}\\[\\033[01;34m\\][\\w]\\n\\[\\033[01;32m\\]\\u@\\h\\[\\033[00m\\]:\\[\\e[33m\\]\\d \\T\\[\\033[00m\\] \\$ "/g' $HOME/.bashrc
+cp -r scripts ~
+cp home/.profile ~
 cd ~/scripts
 chmod 755 clean-history
+sed -i 's/PS1=.*/PS1="${debian_chroot:+($debian_chroot)}\\[\\033[01;34m\\][\\w]\\n\\[\\033[01;32m\\]\\u@\\h\\[\\033[00m\\]:\\[\\e[33m\\]\\d \\T\\[\\033[00m\\] \\$ "/g' $HOME/.bashrc
