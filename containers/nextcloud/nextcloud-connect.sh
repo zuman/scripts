3a473c00160dc77d200c3cd4f6b700c8cfc4ec4b
Zuman ZBS <syed.zuman.007@gmail.com>
2023-09-10 11:23:10 +0000

    Update nextcloud scripts

--- containers/nextcloud/nextcloud-connect.sh
+++ containers/nextcloud/nextcloud-connect.sh
@@ -18,17 +18,24 @@ retries=0
 # Check if Docker is running and wait for the container with retries
 check_docker_and_wait() {
     while [ $retries -lt $max_retries ]; do
-        if docker info &> /dev/null; then
+        if docker info &>/dev/null; then
             # Docker is running
             container_id=$(docker ps -qf "name=$container_name")
 
             if [ -n "$container_id" ]; then
-                # Container is running, attach it to the network
-                if docker network connect "$network_name" "$container_id" &> /dev/null; then
-                    echo "Container $container_name has been attached to $network_name."
-                    return 0  # Success
+                # Container is running
+                if docker network inspect "$network_name" | grep -q "$container_id"; then
+                    # Container is already attached to the network
+                    echo "Container $container_name is already attached to $network_name."
+                    return 0 # Success
                 else
-                    echo "Failed to attach container $container_name to $network_name. Retrying..."
+                    # Container is not attached to the network, attach it
+                    if docker network connect "$network_name" "$container_id" &>/dev/null; then
+                        echo "Container $container_name has been attached to $network_name."
+                        return 0 # Success
+                    else
+                        echo "Failed to attach container $container_name to $network_name. Retrying..."
+                    fi
                 fi
             else
                 echo "Container $container_name is not running. Waiting for it to start..."
@@ -43,13 +50,13 @@ check_docker_and_wait() {
     done
 
     echo "Docker or container did not start within the specified time."
-    return 1  # Failure
+    return 1 # Failure
 }
 
 # Check Docker availability and wait for the container with retries
 if ! check_docker_and_wait; then
-    exit 1  # Script failed
+    exit 1 # Script failed
 fi
 
 # Both Docker and container are ready
-exit 0  # Script succeeded
+exit 0 # Script succeeded
--- containers/nextcloud/nextcloud.sh
+++ containers/nextcloud/nextcloud.sh
@@ -1,16 +1,29 @@
 # https://github.com/nextcloud/all-in-one/blob/main/reverse-proxy.md
 
 docker run \
---sig-proxy=false \
---name nextcloud-aio-mastercontainer \
---restart always \
---publish 8080:8080 \
---env APACHE_PORT=11000 \
---env APACHE_IP_BINDING=0.0.0.0 \
---volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
---volume /var/run/docker.sock:/var/run/docker.sock:ro \
-nextcloud/all-in-one:latest
+    --sig-proxy=false \
+    --name nextcloud-aio-mastercontainer \
+    --restart always \
+    --publish 8080:8080 \
+    --env APACHE_PORT=11000 \
+    --env APACHE_IP_BINDING=0.0.0.0 \
+    --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+    nextcloud/all-in-one:latest
+
+SCRIPT_PATH="$HOME/scripts"
+
+source $SCRIPT_PATH/log.sh
 
 docker network create --attachable proxy-network
 
-docker network connect proxy-network nextcloud-aio-apache
+cp nextcloud-connect.sh $SCRIPT_PATH
+
+(
+    sudo crontab -l
+    echo "@reboot source $SCRIPT_PATH/log.sh; $SCRIPT_PATH/nextcloud-connect.sh | adddate "nextcloud-connect" >> $log"
+) | sudo crontab -
+
+echo "Script $SCRIPT_PATH/nextcloud-connect.sh added to cron for startup."
+
+./nextcloud-connect.sh | adddate "nextcloud-startup" >>$log
